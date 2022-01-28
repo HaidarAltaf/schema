896d1dfbab6d4664af44c67a5527174dcc412a11
HaidarAltaf <97948244+HaidarAltaf@users.noreply.github.com>
2022-01-28 14:21:16 +0000

    Update zoo-schema.sql

--- zoo-schema.sql
+++ zoo-schema.sql
@@ -5,9 +5,14 @@ CREATE TABLE cage (
 animal_name VARCHAR (40), 
 animal_ID INT AUTO_INCREMENT, 
 PRIMARY KEY (animal_ID),
-number_of TINYINT,
-food_necessary BOOLEAN DEFAULT '0',
+number_of INT,
+food_necessary VARCHAR(50) DEFAULT '0',
 Location VARCHAR(40) NOT NULL
 ); 
 
+INSERT INTO cage (animal_name, animal_ID, number_of, food_necessary, Location) VALUES 
+('lion', '1', '25873', 'whale blubber', 'sahara section'); 
+
+
 SELECT * FROM cage; 
+DROP TABLE cage; 
