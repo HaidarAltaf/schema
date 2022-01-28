73aa21ee337ba5cdcf333a87c615e6cb509b7e38
HaidarAltaf <97948244+HaidarAltaf@users.noreply.github.com>
2022-01-28 15:18:33 +0000

    Update zoo-schema.sql

--- zoo-schema.sql
+++ zoo-schema.sql
@@ -10,8 +10,15 @@ food_necessary VARCHAR(50) DEFAULT '0',
 Location VARCHAR(40) NOT NULL
 ); 
 
-INSERT INTO cage (animal_name, animal_ID, number_of, food_necessary, Location) VALUES 
-('lion', '1', '25873', 'whale blubber', 'sahara section'); 
+INSERT INTO animal (animal_name, number_of, food_necessary, Location) VALUES 
+('lion', '25873', 'whale blubber', 'sahara section'),
+('leopard', 12, 'whale blubber', 'sahara section'),
+('kangaroo', '6356', 'pizza', 'crikey'),
+('unicorn', '34565', 'leprechauns', 'hahaha'),
+('killer whale', '6465', 'jupiter', 'athens'),
+('donkey', '1', 'dragons ass', 'swamp'),
+('dragon', '1', 'donkeys family', 'swamp'),
+('sonic', '709', 'failed marriages', 'wifi');
 
 
 SELECT * FROM cage; 
